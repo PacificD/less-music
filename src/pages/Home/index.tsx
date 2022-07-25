0abda012300270f87c0081e41aa37d7e1b658a41
PacificD <1484636442@qq.com>
2022-07-25 17:34:26 +0800

    feat(pages): page 整合playbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-07-18 10:14:40
- * @LastEditTime: 2022-07-25 16:56:12
+ * @LastEditTime: 2022-07-25 17:28:41
  * @LastEditors: Pacific_D
  * @Description:
  * @FilePath: \less-music\src\pages\Home\index.tsx
  */
 
 import { FC } from "react"
-import Header from "../Header"
-import Footer from "../Footer"
+import Header from "./Header"
+import Footer from "./Footer"
 import Main from "./Main"
 
 const Home: FC = () => {
